cad4050e065a2b4359753c5be3025d9fbdaff085
Julien Hillairet <julien.hillaire@cea.fr>
2016-11-29 17:17:41 +0100

    Working voltage&current calculation for the CEA branch.

--- tresonator/configuration.py
+++ tresonator/configuration.py
@@ -4,7 +4,8 @@ Configuration of the T-resonator
 """
 from . constants import *
 from . coaxial import Coax
-from . transmission_line_utils import ZL_2_Zin
+from . transmission_line_utils import ZL_2_Zin, transfer_matrix
+import numpy as np
 
 class Configuration(object):
     def __init__(self, f, P_in, L_DUT, L_CEA, additional_losses=1):
@@ -23,12 +24,15 @@ class Configuration(object):
         self.Z_short_DUT = 0.0052 
         self.Z_short_CEA = 0.0087 
         
+        # feeder impedance 
+        self.R = 29.8 # Ohm
+        
         # relative additional loss coefficient to multiply with the loss in the
         # coaxial transmission line, in order to match measurements 
         # example : +20# losses --> 1.2
         self.additional_losses = additional_losses 
         
-        self.TLs, self.alphas = self._resonator_config()
+        self.TLs, self.gammas = self._resonator_config()
 
     def __repr__(self):
         return 'T-resonator config: f={} MHz, P_in={} kW, L_DUT={} m, L_CEA={} m'.format( \
@@ -82,14 +86,12 @@ class Configuration(object):
         
         # Calculate the losses for each section
         
-        alphas = []        
+        gammas = []        
         for TL in TLs:
-            alpha = TL.alpha(self.f)
-            # artifically increases the conduction losses in order to match experiment
-            new_alpha = self.additional_losses*alpha.real + 1j*alpha.imag
-            alphas.append(new_alpha)
+            gamma = TL.gamma(self.f, self.additional_losses)
+            gammas.append(gamma)
             
-        return TLs, alphas
+        return TLs, gammas
 
 
     def input_impedance(self):
@@ -102,27 +104,25 @@ class Configuration(object):
         
         Returns
         -------
-        Zin : input impedance of the T-resonator
-        Z_CEA [optionnal] : input impedances at each sections of the CEA
-                               branch, from short to T
-        Z_DUT [optionnal] : input impedances at each sections of the DUT
-                               branch, from short to T   
+        Zin:    input impedance of the T-resonator
+        Z_CEA:  input impedances at each sections of the CEA branch, from short to T
+        Z_DUT:  input impedances at each sections of the DUT branch, from short to T   
         """        
         
         # DUT BRANCH
         Z_DUT = []
-        Z_DUT.append(ZL_2_Zin(self.TLs[0].L, self.TLs[0].Zc, self.TLs[0].gamma(self.f), self.Z_short_DUT))
-        Z_DUT.append(ZL_2_Zin(self.TLs[1].L, self.TLs[1].Zc, self.TLs[1].gamma(self.f), Z_DUT[0]))
-        Z_DUT.append(ZL_2_Zin(self.TLs[2].L, self.TLs[2].Zc, self.TLs[2].gamma(self.f), Z_DUT[1]))
-        Z_DUT.append(ZL_2_Zin(self.TLs[3].L, self.TLs[3].Zc, self.TLs[3].gamma(self.f), Z_DUT[2]))
-        Z_DUT.append(ZL_2_Zin(self.TLs[4].L, self.TLs[4].Zc, self.TLs[4].gamma(self.f), Z_DUT[3]))
+        Z_DUT.append(ZL_2_Zin(self.TLs[0].L, self.TLs[0].Zc, self.gammas[0], self.Z_short_DUT))
+        Z_DUT.append(ZL_2_Zin(self.TLs[1].L, self.TLs[1].Zc, self.gammas[1], Z_DUT[0]))
+        Z_DUT.append(ZL_2_Zin(self.TLs[2].L, self.TLs[2].Zc, self.gammas[2], Z_DUT[1]))
+        Z_DUT.append(ZL_2_Zin(self.TLs[3].L, self.TLs[3].Zc, self.gammas[3], Z_DUT[2]))
+        Z_DUT.append(ZL_2_Zin(self.TLs[4].L, self.TLs[4].Zc, self.gammas[4], Z_DUT[3]))
         
         # CEA BRANCH
         Z_CEA = []
-        Z_CEA.append(ZL_2_Zin(self.TLs[8].L, self.TLs[8].Zc, self.TLs[8].gamma(self.f), self.Z_short_CEA))# 9
-        Z_CEA.append(ZL_2_Zin(self.TLs[7].L, self.TLs[7].Zc, self.TLs[7].gamma(self.f), Z_CEA[0])) # 8
-        Z_CEA.append(ZL_2_Zin(self.TLs[6].L, self.TLs[6].Zc, self.TLs[6].gamma(self.f), Z_CEA[1])) # 7
-        Z_CEA.append(ZL_2_Zin(self.TLs[5].L, self.TLs[5].Zc, self.TLs[5].gamma(self.f), Z_CEA[2])) # 6
+        Z_CEA.append(ZL_2_Zin(self.TLs[8].L, self.TLs[8].Zc, self.gammas[8], self.Z_short_CEA))# 9
+        Z_CEA.append(ZL_2_Zin(self.TLs[7].L, self.TLs[7].Zc, self.gammas[7], Z_CEA[0])) # 8
+        Z_CEA.append(ZL_2_Zin(self.TLs[6].L, self.TLs[6].Zc, self.gammas[6], Z_CEA[1])) # 7
+        Z_CEA.append(ZL_2_Zin(self.TLs[5].L, self.TLs[5].Zc, self.gammas[5], Z_CEA[2])) # 6
         
         # At T-junction. Impedance are associated in parallel.
         Zin = (Z_DUT[-1]*Z_CEA[-1])/(Z_DUT[-1] + Z_CEA[-1])
@@ -130,3 +130,96 @@ class Configuration(object):
         return Zin, Z_CEA, Z_DUT
         
         
+    def voltage_current(self):
+        """ 
+        Returns the voltage and current along the resonator. 
+        
+        Returns
+        -------
+        L_CEA: length 
+        L_DUT:
+        V_CEA:
+        V_DUT: 
+        I_CEA:
+        I_DUT:
+        """
+        Zin, Z_CEA, Z_DUT = self.input_impedance()
+        
+        # calculate the voltage and current along the T-resonator branches
+        L_CEA, V_CEA, I_CEA, Z_CEA = self._voltage_current_CEAbranch(Zin, Z_CEA)
+        L_DUT, V_DUT, I_DUT, Z_DUT = self._voltage_current_CEAbranch(Zin, Z_DUT)
+        
+        return L_CEA, L_DUT, V_CEA, V_DUT, I_CEA, I_DUT
+        
+    def _voltage_current_CEAbranch(self, Zin, Z_CEA):
+        # spatial sampling 
+        dl = 1e-3   
+        # Input voltage from input power and feeder impedance
+        Vin = np.sqrt(self.P_in*2*self.R) # forward voltage
+        rho_in = (Zin - self.R)/(Zin + self.R) # reflection coefficient
+        
+        # arrays of initial voltage and current for each line section
+        V0, I0 = [], []
+        # arrays along L
+        V, I, Z, L_full = [], [], [], []
+
+        # Going from T to short 
+        V0.append( Vin*(1 + rho_in) ) # total voltage
+        I0.append( V0[0]/Z_CEA[3] )
+        _L = np.arange(start=0, stop=self.TLs[5].L, step=dl)         
+        for l in _L:
+            _V, _I = transfer_matrix(-l, V0[-1], I0[-1], self.TLs[5].Zc, self.gammas[5])
+            V.append(_V)
+            I.append(_I)
+            Z.append(_V/_I)
+        L_full.append(_L)
+        
+        V0.append( V[-1] )
+        I0.append( I[-1] )
+        _L = np.arange(start=0, stop=self.TLs[6].L, step=dl)
+        for l in _L:
+            _V, _I = transfer_matrix(-l, V0[-1], I0[-1], self.TLs[6].Zc, self.gammas[6])
+            V.append(_V)
+            I.append(_I)
+            Z.append(_V/_I)
+        L_full.append(_L + L_full[-1][-1])                                    
+ 
+        V0.append( V[-1] )
+        I0.append( I[-1] )
+        _L = np.arange(start=0, stop=self.TLs[6].L, step=dl)
+        for l in _L:
+            _V, _I = transfer_matrix(-l, V0[-1], I0[-1], self.TLs[6].Zc, self.gammas[6])
+            V.append(_V)
+            I.append(_I)
+            Z.append(_V/_I)
+        L_full.append( _L + L_full[-1][-1] )                                      
+        
+        V0.append( V[-1] )
+        I0.append( I[-1] )
+        _L = np.arange(start=0, stop=self.TLs[7].L, step=dl)
+        for l in _L:
+            _V, _I = transfer_matrix(-l, V0[-1], I0[-1], self.TLs[6].Zc, self.gammas[7])
+            V.append(_V)
+            I.append(_I)
+            Z.append(_V/_I)
+        L_full.append( _L + L_full[-1][-1] )   
+  
+        V0.append( V[-1] )
+        I0.append( I[-1] )
+        _L = np.arange(start=0, stop=self.TLs[8].L, step=dl)
+        for l in _L:
+            _V, _I = transfer_matrix(-l, V0[-1], I0[-1], self.TLs[6].Zc, self.gammas[8])
+            V.append(_V)
+            I.append(_I)
+            Z.append(_V/_I)
+        L_full.append( _L + L_full[-1][-1] )  
+        
+        # convert list into arrays
+        V = np.asarray(V)
+        I = np.asarray(I)
+        Z = np.asarray(Z)
+        L = np.concatenate(np.asarray([np.asarray(_L) for _L in L_full]))
+
+        
+        return L, V, I, Z
+    
