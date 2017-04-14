f3e794437d58a13c2b9be567d61a70995c0738ff
Julien Hillairet <julien.hillaire@cea.fr>
2017-04-14 21:37:04 +0200

    Test if the solution found is physical (respect the min and max short lengths)

--- SSA50_resonator_optimize_lengths.py
+++ SSA50_resonator_optimize_lengths.py
@@ -9,19 +9,24 @@ import tresonator as T
 import matplotlib.pyplot as plt
 import numpy as np
 
-f = 62.90e6 # Hz
-P_in = 80e3 # W
+f = 62.64e6 # Hz
+P_in = 20e3 # W
 add_loss = 1.0
 
 # setup the initial resonator configuration, in which L_DUT and L_CEA
 # are not the necessary optimum values
-Lsc_DUT = 0.0624 # m
-Lsc_CEA = 0.00224 # m
+Lsc_DUT = 0.05 # m
+Lsc_CEA = 0.03 # m
 
 cfg = T.Configuration(f, P_in, Lsc_DUT, Lsc_CEA, additional_losses=add_loss)
 
 # Solve the optimization problem
 L_opt = cfg.optimize_short_lengths()
+while True: # repeat searches until a physical meaningfull solution is found...
+    if L_opt[0] < 20e-3 or L_opt[0] > 60e-3:
+        L_opt = cfg.optimize_short_lengths()
+    else:
+        break
 
 # Calculates the S11 for a range of frequency
 freqs = np.linspace(61e6, 64e6, 301)
