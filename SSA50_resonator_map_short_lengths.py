8a7c01515fc263c0021b9718ac9078c851600569
Julien Hillairet <julien.hillaire@cea.fr>
2018-01-12 13:35:09 +0100

    minor changes in the parameters used for simulation.

--- SSA50_resonator_map_short_lengths.py
+++ SSA50_resonator_map_short_lengths.py
@@ -7,13 +7,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-f = 62.90e6 # Hz
+f = 62.64e6 # Hz
 P_in = 80e3 # W
 add_loss = 1
 
 # short lengths to map
-L_CEA = np.linspace(1e-3, 0.2, num=201)
-L_DUT = np.linspace(1e-3, 0.1, num=201)
+L_DUT = np.linspace(1e-3, 0.06, num=201)
+L_CEA = np.linspace(1e-3, 0.125, num=201)
 
 # calculates the S11 and Zin for all the short lengths
 S11dB = np.zeros( (len(L_DUT), len(L_CEA)) )
@@ -26,13 +26,13 @@ for idx_DUT, _L_DUT in enumerate(L_DUT):
         _Zin, _ , _ = _cfg.input_impedance()
         Zin[idx_DUT, idx_CEA] = _Zin
 
-# Plotting S11 vs L_DUT and L_CEA        
+#%% Plotting S11 vs L_DUT and L_CEA        
 f,ax=plt.subplots(1,1)
 cx=ax.contourf(L_DUT, L_CEA, S11dB.T, 50, cmap='afmhot')
 plt.colorbar(cx)
 ax.set_xlabel('L DUT [m]', fontsize=14)
 ax.set_ylabel('L CEA [m]', fontsize=14)
-
+#%% plot the location of the optimized values
 L = _cfg.optimize_short_lengths()
 ax.axvline(L[0], color='gray')
 ax.axhline(L[1], color='gray')
